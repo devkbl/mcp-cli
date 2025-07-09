a33868ed52fc10ee579eed348e02b8700c7eb30b
Kieran Barty <kieran.barty@outlook.com>
2025-07-09 21:59:18 +0100

    feat(view): enabled debug logs, and got list sizes set correctly "

--- cmd/view/main.go
+++ cmd/view/main.go
@@ -11,6 +11,25 @@ import (
 func main() {
 	// list of tabs to display
 
+	// setup default logger
+
+	// logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	// slog.SetDefault(logger)
+
+	slog.SetLogLoggerLevel(slog.LevelDebug)
+	// TODO: get this from a flag being passed in but for now hardcode
+
+	debug := true
+	if debug {
+		f, err := tea.LogToFile("debug.log", "")
+		if err != nil {
+			slog.Default().Error("error starting logger", slog.Any("error", err))
+			os.Exit(1)
+		}
+
+		defer f.Close()
+	}
+
 	m := view.NewModel()
 	if _, err := tea.NewProgram(m).Run(); err != nil {
 		slog.Default().Error("error running cli", slog.Any("error", err))
--- internal/app/view/list.go
+++ internal/app/view/list.go
@@ -3,29 +3,55 @@ package view
 import (
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+// define custom int type to let us refer to list slice elements by a descriptive name
+type listname int
+
+// list names
+const (
+	clients   listname = iota // 0
+	methods                   // 1
+	responses                 // 2
 )
 
 // initial main model
 type Model struct {
-	list list.Model
+	// slice of models - client | methods | responses
+	lists []list.Model
+	// focused refers to the currently selected list in the above array - TODO: define custom type?
+	focused listname
 }
 
 func NewModel() Model {
-	// initialize main models list
-
 	m := Model{}
 
-	// TODO: set height somehow ?
-	m.list = list.New([]list.Item{}, list.NewDefaultDelegate(), 0, 0)
-
-	// set the title of the initial, main list
-	m.list.Title = "MCP Clients"
-
-	// initialize the list of items, using our client items.
-	m.list.SetItems([]list.Item{
+	// initialise client list
+	clientList := list.New([]list.Item{}, list.NewDefaultDelegate(), 0, 0)
+	clientList.Title = "MCP Clients"
+	clientList.SetItems([]list.Item{
 		client{name: "test-client", description: "test, fake mcp client", url: "http://localhost:8080"},
 		client{name: "test-client-2", description: "test-2, fake mcp client", url: "http://localhost:8080"},
 	})
+
+	// initialize methods list
+	methodsList := list.New([]list.Item{}, list.NewDefaultDelegate(), 0, 0)
+	methodsList.Title = "Methods"
+
+	// TODO: remove this
+	methodsList.SetItems([]list.Item{
+		client{name: "test-method", description: "test fake method", url: "no url"},
+	})
+	// initialize responses list
+	responsesList := list.New([]list.Item{}, list.NewDefaultDelegate(), 0, 0)
+	responsesList.Title = "responses"
+
+	m.lists = []list.Model{
+		clientList,
+		methodsList,
+		responsesList,
+	}
 
 	return m
 }
@@ -39,16 +65,20 @@ func (m Model) Init() tea.Cmd {
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
-		m.list.SetHeight(msg.Height)
-		m.list.SetWidth(msg.Width)
+		// set all list sizes
+		for i := 0; i < len(m.lists); i++ {
+			m.lists[i].SetHeight(msg.Height)
+			m.lists[i].SetWidth(msg.Width)
+		}
 	}
 	// pass update to inner list model
 	var cmd tea.Cmd
-	m.list, cmd = m.list.Update(msg)
+	m.lists[m.focused], cmd = m.lists[m.focused].Update(msg)
 	return m, cmd
 }
 
 // update the view based on the current model state
 func (m Model) View() string {
-	return m.list.View()
+	// join multiple lists in the view using lipgloss, start with first
+	return lipgloss.JoinHorizontal(lipgloss.Left, m.lists[clients].View(), m.lists[methods].View(), m.lists[responses].View())
 }
